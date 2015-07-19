f3162008fe8332e97688d89e8d624f82e86c0127
Lasana Murray <lmurray@quenk.com>
2015-07-19 17:26:38 -0400

    Update Connections API.

--- src/connections/Connections.js
+++ src/connections/Connections.js
@@ -1,89 +1,65 @@
-var Promise = require('bluebird');
+import Promise from 'bluebird';
 var UnknownConnectionError = require('./UnknownConnectionError');
 var UnsupportedConnectionTypeError = require('./UnsupportedConnectionTypeError');
 
 /**
- * Connections provides a factory for creating supported connections.
+ * Connections is a registry for connections
  */
-function Connections() {
+class Connections {
 
-    this.types = {
-        'mongoose': require('./Mongoose'),
-        'connect-mongo': require('./ConnectMongo')
+    constructor() {
+        this.types = {};
+        this.pool = {};
     }
 
-    this.list = [];
+    /**
+     * create will return an instance of the desired connection if found.
+     * @param {String} type
+     * @param {String} name
+     * @param {Object} options
+     * @returns {Connection}
+     */
+    create(type, name, options) {
 
+        var conn;
+
+        if (type in this.types)
+            conn =  this.types[type].create(name, options, this.list);
+
+        this.pool[name] = conn;
+        return conn;
+
+    }
+
+
+    /**
+     * getConnection
+     * @param {String} name
+     * @return {Connection}
+     */
+    getConnection(name) {
+      return this.pool[name];
+    }
+
+    /**
+     * open
+     * @return {Promise}
+     */
+    open() {
+        return Promise.all(Object.keys(this.pool).map(function (connection) {
+            return connection.open()
+        }));
+    }
+
+    /**
+     * close all the connections
+     * @return {Promise}
+     */
+    close() {
+        return Promise.all(Object.keys(this.pool).map(function (connection) {
+            return connection.close()
+        }));
+    }
 }
 
-/**
- * create will return an instance of the desired connection if found.
- * @param {String} name
- * @param {String} type
- * @param {Object} options
- * @returns {Connection}
- */
-Connections.prototype.create = function (name, type, options) {
-
-    if (type in this.types)
-        return new this.types[type](name, options, this.list);
-
-    throw new UnsupportedConnectionTypeError(name);
-
-};
-
-/**
- * hasName
- * @return {Boolean}
- */
-Connections.prototype.hasName = function (name) {
-
-    var ret;
-
-    this.list.forEach(function (connection) {
-        if (connection.name === name)
-            ret = true;
-    });
-
-    return ret;
-};
-/**
- * getByName returns a connection based on its assigned name.
- * @param {Connection}
- */
-Connections.prototype.getByName = function (name) {
-
-    var ret;
-
-    this.list.forEach(function (connection) {
-
-        if (connection.name === name)
-            ret = connection;
-    })
-
-    if (!ret)
-        throw new UnknownConnectionError(name);
-
-    return ret;
-
-};
-
-/**
- * open all the connections.
- */
-Connections.prototype.open = function () {
-    return Promise.all(this.list.map(function (connection) {
-        return connection.open()
-    }));
-};
-
-/**
- * close all the connections
- */
-Connections.prototype.close = function () {
-    return Promise.all(this.list.map(function (connection) {
-        return connection.close();
-    }));
-};
-
-module.exports = new Connections();
+export default new Connections();

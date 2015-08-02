2ac80a6e5523cbf26e0b7a9e043a6d15fa63fcc5
Lasana Murray <lmurray@quenk.com>
2015-08-02 16:54:11 -0400

    connect-mongo -> use mongoose connectinos

--- src/conn/mongo/ConnectMongoConnection.js
+++ src/conn/mongo/ConnectMongoConnection.js
@@ -1,6 +1,7 @@
 import AbstractConnection from '../AbstractConnection';
 import session from 'express-session';
 import cmongo from  'connect-mongo';
+import Connections from '../Connections';
 
 class ConnectMongoConnection extends AbstractConnection {
 
@@ -12,9 +13,15 @@ class ConnectMongoConnection extends AbstractConnection {
     }
 
     __open__(resolve, reject) {
+
         var MongoConnection = cmongo(session);
+
+        if(typeof this.options.store === 'string')
+        this.options.store = Connections.getConnection(this.options.store).connection;
+
         this.connection = new MongoConnection(this.options);
         resolve(this.connection);
+
     }
 
     __close__(resolve, reject) {
